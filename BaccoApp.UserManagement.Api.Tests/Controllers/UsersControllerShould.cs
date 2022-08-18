53b8ac3fe259627066f01d56aa360e9d098ed87a
Juan Sebastian Ramirez Solarte <juan.solarte@ceiba.com.co>
2022-08-18 08:20:27 -0500

    Unit test api

---
+++ BaccoApp.UserManagement.Api.Tests/Controllers/UsersControllerShould.cs
@@ -0,0 +1,93 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using BaccoApp.UserManagement.Api.Controllers;
+using BaccoApp.UserManagement.Application;
+using BaccoApp.UserManagement.Application.CreateUser;
+using BaccoApp.UserManagement.Application.Users.Dtos;
+using BaccoApp.UserManagement.Application.Users.GetUsers;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Net;
+using Xunit;
+
+namespace BaccoApp.UserManagement.Api.Tests.Controllers
+{
+    public class UsersControllerShould
+    {
+        private readonly IFixture _fixture;
+
+        public UsersControllerShould()
+        {
+            _fixture = new Fixture().Customize(new AutoMoqCustomization());
+        }
+
+        [Fact]
+        public async Task CreateUserShouldUseMediator_Return201StatusCodeAsync()
+        {
+            //Arrange
+
+            var mediatrMock = _fixture.Freeze<Mock<IMediator>>();
+            mediatrMock.Setup(_ => _.Send(It.IsAny<IRequest<DetailUserDto>>(), It.IsAny<CancellationToken>())).ReturnsAsync(new DetailUserDto());
+
+            var sut = _fixture.Build<UsersController>().OmitAutoProperties().Create();
+
+            //Act
+
+            var response = await sut.CreateUser(new CreateUserCommand());
+
+            //Assert
+
+            mediatrMock.Verify(_ => _.Send(It.IsAny<IRequest<DetailUserDto>>(), It.IsAny<CancellationToken>()), Times.Once);
+            response.Should().NotBeNull();
+            response.Result.Should().BeOfType<CreatedAtActionResult>().Which.ActionName.Should().Be("CreateUser");
+            response.Result.Should().BeOfType<CreatedAtActionResult>().Which.StatusCode.Should().Be((int)HttpStatusCode.Created);
+            response.Result.Should().BeOfType<CreatedAtActionResult>().Which.Value.Should().BeOfType<DetailUserDto>();
+        }
+
+        [Fact]
+        public async Task UpdateUserShouldUseMediator_Return200StatusCodeAsync()
+        {
+            //Arrange
+
+            var mediatrMock = _fixture.Freeze<Mock<IMediator>>();
+            mediatrMock.Setup(_ => _.Send(It.IsAny<IRequest<DetailUserDto>>(), It.IsAny<CancellationToken>())).ReturnsAsync(new DetailUserDto());
+
+            var sut = _fixture.Build<UsersController>().OmitAutoProperties().Create();
+
+            //Act
+
+            var response = await sut.UpdateUser(Guid.NewGuid(), new EditableUserDto());
+
+            //Assert
+
+            mediatrMock.Verify(_ => _.Send(It.IsAny<IRequest<DetailUserDto>>(), It.IsAny<CancellationToken>()), Times.Once);
+            response.Should().NotBeNull();
+            response.Result.Should().BeOfType<OkObjectResult>().Which.StatusCode.Should().Be((int)HttpStatusCode.OK);
+            response.Result.Should().BeOfType<OkObjectResult>().Which.Value.Should().BeOfType<DetailUserDto>();
+        }
+
+        [Fact]
+        public async Task GetUsersShouldUseMediator_Return200StatusCodeAsync()
+        {
+            //Arrange
+
+            var mediatrMock = _fixture.Freeze<Mock<IMediator>>();
+            mediatrMock.Setup(_ => _.Send(It.IsAny<IRequest<PaginationResponse<ListUserDto>>>(), It.IsAny<CancellationToken>())).ReturnsAsync(new PaginationResponse<ListUserDto>(new PaginationRequest(), new List<ListUserDto>(), 0));
+
+            var sut = _fixture.Build<UsersController>().OmitAutoProperties().Create();
+
+            //Act
+
+            var response = await sut.GetUsers(new GetUsersQuery());
+
+            //Assert
+
+            mediatrMock.Verify(_ => _.Send(It.IsAny<IRequest<PaginationResponse<ListUserDto>>>(), It.IsAny<CancellationToken>()), Times.Once);
+            response.Should().NotBeNull();
+            response.Result.Should().BeOfType<OkObjectResult>().Which.StatusCode.Should().Be((int)HttpStatusCode.OK);
+            response.Result.Should().BeOfType<OkObjectResult>().Which.Value.Should().BeOfType<PaginationResponse<ListUserDto>>();
+        }
+    }
+}
---
+++ BaccoApp.UserManagement.Api.Tests/Filters/AppExceptionFilterAttributeShould.cs
@@ -0,0 +1,71 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using BaccoApp.UserManagement.Api.Filters;
+using BaccoApp.UserManagement.Domain.Exceptions;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using System.Net;
+using Xunit;
+
+namespace BaccoApp.UserManagement.Api.Tests.Filters
+{
+    public class AppExceptionFilterAttributeShould
+    {
+        private readonly IFixture fixture;
+
+        public AppExceptionFilterAttributeShould()
+        {
+            fixture = new Fixture().Customize(new AutoMoqCustomization());
+        }
+
+        [Fact]
+        public void ChangeStatusCodeTo400WhenIsAppException()
+        {
+            // Arrange
+            var appExceptionFilterAttribute = fixture.Create<AppExceptionFilterAttribute>();
+            fixture.Customize<ActionContext>(_ => _.OmitAutoProperties()
+                .With(_ => _.HttpContext, new DefaultHttpContext())
+                .With(_ => _.RouteData, new RouteData())
+                .With(_ => _.ActionDescriptor, new ActionDescriptor()));
+
+            var resultExecutingContext = fixture.Build<ExceptionContext>()
+                .OmitAutoProperties()
+                .With(_ => _.Exception, new AppException())
+                .Create();
+
+            // Act
+            appExceptionFilterAttribute.OnException(resultExecutingContext);
+
+            // Assert
+            resultExecutingContext.HttpContext.Response.StatusCode.Should().Be((int)HttpStatusCode.BadRequest);
+            resultExecutingContext.Result.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void ChangeStatusCodeTo500WhenIsGenericException()
+        {
+            // Arrange
+            var appExceptionFilterAttribute = fixture.Create<AppExceptionFilterAttribute>();
+            fixture.Customize<ActionContext>(_ => _.OmitAutoProperties()
+                .With(_ => _.HttpContext, new DefaultHttpContext())
+                .With(_ => _.RouteData, new RouteData())
+                .With(_ => _.ActionDescriptor, new ActionDescriptor()));
+
+            var resultExecutingContext = fixture.Build<ExceptionContext>()
+                .OmitAutoProperties()
+                .With(_ => _.Exception, new Exception())
+                .Create();
+
+            // Act
+            appExceptionFilterAttribute.OnException(resultExecutingContext);
+
+            // Assert
+            resultExecutingContext.HttpContext.Response.StatusCode.Should().Be((int)HttpStatusCode.InternalServerError);
+            resultExecutingContext.Result.Should().NotBeNull();
+        }
+    }
+}
